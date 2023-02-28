c51e6b8214904da3dd78213067fb57f5178fa13a
kalpana <kalpanarajasekar27@gmail.com>
2023-02-28 23:42:38 +0000

    updated js file

--- README.md
+++ README.md
@@ -1,2 +1,57 @@
-# README-generator
-Professional README file generator
+# README generator
+  ![Github license](https://img.shield.io/badge/MIT-blue.svg)
+
+  ### Description
+
+  professional README
+
+  ### Table of contents
+
+  *[Installation](#installation)
+
+  *[Usage](#usage)
+
+  
+* [License](#license)
+
+
+  * [Contributing](#contributing)
+
+  *[Test] (#tests)
+
+  *[Questions] (#questions)
+
+  ##Installation
+
+  To install necessary dependencies, run the following command:
+
+  ```
+
+  npm i
+
+  ```
+
+  # Usage
+
+  fork
+
+  ## License
+
+  ## Contributing
+
+  fork
+
+  ## Tests
+
+  To run tests, run following command:
+
+  ```
+  npm test
+  ```
+
+  ### Questions
+
+  If you have any questions about the repo open an issue or contact me directly as kalpanarajasekar27@gmail.com.
+
+  You can find more of my work at [Kalpana-Sivaprakash](https://github.com/Kalpana-Sivaprakash).
+
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,81 @@
 // function to generate markdown for README
+const renderLicenseBadge = (license) => {
+  if(license !== "None") {
+    return `![Github license](https://img.shield.io/badge/${license}-blue.svg)`
+    return ''
+  }
+}
+
+const renderLicenseLink = (license) => {
+  if(license!=="None") {
+
+    return `\n* [License](#license)\n`
+
+  }
+
+  return '';
+}
+
 function generateMarkdown(data) {
   return `# ${data.title}
+  ${renderLicenseBadge(data.license)}
+
+  ### Description
+
+  ${data.description}
+
+  ### Table of contents
+
+  *[Installation](#installation)
+
+  *[Usage](#usage)
+
+  ${renderLicenseLink (data.license)}
+
+  * [Contributing](#contributing)
+
+  *[Test] (#tests)
+
+  *[Questions] (#questions)
+
+  ##Installation
+
+  To install necessary dependencies, run the following command:
+
+  \`\`\`
+
+  ${data.installation}
+
+  \`\`\`
+
+  # Usage
+
+  ${data.usage}
+
+  ## License
+
+  This project is licensed under the ${data.license} license.
+
+  ## Contributing
+
+  ${data.contributing}
+
+  ## Tests
+
+  To run tests, run following command:
+
+  \`\`\`
+  ${data.test}
+  \`\`\`
+
+  ### Questions
+
+  If you have any questions about the repo open an issue or contact me directly as ${data.email}.
+
+  You can find more of my work at [${data.github}](https://github.com/${data.github}).
 
 `;
 }
 
 module.exports = generateMarkdown;
+
